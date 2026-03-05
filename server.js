817ffb6a3bc75e8b16f8f0a6d6ece9e0b9152ae6
Krk-99 <krisrk2009@gmail.com>
2026-03-04 23:09:07 -0500

    Added a Database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose")
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,8 +10,13 @@ app.use(cors());
 
 app.use(express.json());
 
+const mongoURI = process.env.MONGODB_URI;
+mongoose.connect(mongoURI)
+    .then(() => console.log("Connected to MongoDB Atlas"))
+    .catch(err => console.error("Could not connect to MongoDB: ", err));
+
 app.post("/api/flight", (req, res) => {
-    const recdata = req.body.info
+    const recdata   = req.body.info
     res.status(200).json({
         message: 'Data recieved successfully',
         displaytext: recdata + " from the backend",
