d47ae2432ce5d3738a1730ddbe49d982cd2d8873
Krk-99 <krisrk2009@gmail.com>
2026-03-05 06:46:43 -0500

    Authentication Initial Format

--- server.js
+++ server.js
@@ -15,13 +15,32 @@ mongoose.connect(mongoURI)
     .then(() => console.log("Connected to MongoDB Atlas"))
     .catch(err => console.error("Could not connect to MongoDB: ", err));
 
-app.post("/api/flight", (req, res) => {
-    const recdata   = req.body.info
-    res.status(200).json({
-        message: 'Data recieved successfully',
-        displaytext: recdata + " from the backend",
-        data: recdata       
-    })  
+const userschema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+const User = mongoose.model("User", userschema);
+
+
+app.post("/api/flight", async (req, res) => {
+    try {
+        const {username, password} = req.body;
+        const foundUser = await User.findOne({username: username})
+        if (!foundUser) return res.status(404).json({message: "User not found"})
+        if (password === foundUser.password) {
+            res.status(200).json({message: "Access Granted"})
+        } else {
+            res.status(200).json({message: "Access Denied"})
+        }
+        // res.status(200).json({
+        //     message: 'Data recieved successfully',
+        //     displaytext: recdata + " from the backend",
+        //     data: recdata       
+        // })  
+    } catch (error) {
+        res.status(500).json({message: "Server Error", error})
+    }
+
 });
 
 app.listen(port, () => {
