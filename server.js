e8918b287139d6a15947cf75f0c9a23ca7a88b1b
Krk-99 <krisrk2009@gmail.com>
2026-03-23 23:05:31 -0400

    Task Creator V1

--- server.js
+++ server.js
@@ -35,6 +35,23 @@ const userschema = new mongoose.Schema({
 });
 const User = mongoose.model("User", userschema);
 
+const taskschema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        default: 'No description'
+    },
+    userid: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    }
+});
+
+const Task = mongoose.model("Task", taskschema)
 
 app.post("/api/flight", async (req, res) => {
     try {
@@ -44,6 +61,7 @@ app.post("/api/flight", async (req, res) => {
         if (password === foundUser.password) {
             req.session.isLoggedIn = true;
             req.session.user = foundUser.username;
+            req.session.userId = foundUser._id;
             req.session.save((err) => {
                 if(err) {
                     return res.status(500).json({message: "Error Saving Please Try Again"})
@@ -61,6 +79,22 @@ app.post("/api/flight", async (req, res) => {
         res.status(500).json({message: "Server Error", error})
     }
 });
+
+app.post("/api/newtask", async (req, res) => {
+    try {
+        const {title, description} = req.body;
+        const newTask = new Task({
+            title: title,
+            description: description,
+            userid: req.session.userId
+        })
+        await newTask.save()
+        res.status(200).json({message: "Task succesfully created"})
+    } catch (error){
+        console.log(error)
+        res.status(400).json({message: "Server error, Please Contact Admin"})
+    }
+})
 
 const checkAuth = (req, res, next) => {
     if (req.session.isLoggedIn) {
